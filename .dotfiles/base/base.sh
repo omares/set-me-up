d56b8e329fce7d6e6c98758b753b1464446f0b16
Ota Mares <ota@rebuy.com>
2018-04-28 01:04:11 +0200

    Improve first time installation & try to simplify smu

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -26,4 +26,11 @@ echo "------------------------------"
 # rcup is used to install files from the tag-specific dotfiles directory.
 # rcup is part of rcm, a management suite for dotfiles
 # Check https://github.com/thoughtbot/rcm for more info
-env RCRC=".rcrc" rcup
+
+# get the absolute path of the .dotfiles directory
+# this is only for aesthetic reasons to have an absolute symlink path instead of a relative one
+# <path-to-smu>/.dotfiles/somedotfile vs <path-to-smu>/.dotfiles/base/../somedotfile
+readonly dotfiles="$(dirname -- "$(dirname -- "$(readlink -f -- "$0")")")"
+
+export RCRC="../rcrc"
+rcup -v -d "${dotfiles}"
