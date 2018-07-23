91a0d27d19f18e6676cd56fa266d06d50ab4e577
Ota Mares <ota@rebuy.com>
2018-07-23 22:00:32 +0200

    Fix #23 - Move sudo enforcement into child scripts

--- .dotfiles/tag-smu/modules/macosupdate/macosupdate.sh
+++ .dotfiles/tag-smu/modules/macosupdate/macosupdate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ "${EUID}" -ne 0 ]]; then
-  echo "This module requires sudo rights."
-  echo "You should run this module using the smuu script."
-  exit 1
-fi
-
 echo "------------------------------"
 echo "Updating macOS. If this requires a restart, run the script again."
 echo "------------------------------"
 echo ""
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install all available updates
 sudo softwareupdate -i -a
